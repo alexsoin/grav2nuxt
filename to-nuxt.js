59de83599cd9580813aa0bf13626351a7089ea6d
Alex Soin <alex94soin@gmail.com>
2022-02-01 23:41:06 +0400

    feat: update date

--- to-nuxt.js
+++ to-nuxt.js
@@ -6,10 +6,12 @@ const birthDate = require('file-birth');
 const json2yaml = require('json2yaml').stringify;
 const parseMD = require('parse-md').default;
 
-const pathGrav = path.join(__dirname, "grav");
+const dirInport = "pages"
+const pathGrav = path.join(__dirname, "pages");
 const pathNuxt = {
-    img: path.join(__dirname, "nuxt", "assets", "img"),
-    content: path.join(__dirname, "nuxt", "content")
+	img: path.join(__dirname, "nuxt", "assets", "img"),
+	content: path.join(__dirname, "nuxt", "content"),
+	posters: path.join(__dirname, "nuxt", "posters"),
 };
 
 /**
@@ -17,11 +19,11 @@ const pathNuxt = {
  * @param {String} source путь
  * @returns {Array}
  */
-const getDirectories = source => 
-    fs.readdirSync(source, {withFileTypes: true})
-        .filter(dirent => dirent.isDirectory())
-        .map(dirent => path.join(source, dirent.name));
-    
+const getDirectories = source =>
+	fs.readdirSync(source, {withFileTypes: true})
+		.filter(dirent => dirent.isDirectory())
+		.map(dirent => path.join(source, dirent.name));
+
 
 const getListImagesInDir = (dir) => fs.readdirSync(dir).filter(elem => elem.match(/(jpg|jpeg|png|gif)$/)).map(elem => elem);
 
@@ -31,13 +33,13 @@ const getListImagesInDir = (dir) => fs.readdirSync(dir).filter(elem => elem.matc
  * @returns {Array}
  */
 const generateList = list => {
-    return list.map(item => {
-        return {
-            name: item.split('.')[1] || item.split('/')[1] || item,
-            dir: item,
-            outDir: item.split('.')[1] ? item.split('/')[0] + "/" + item.split('.')[1] : item
-        }
-    })
+	return list.map(item => {
+		return {
+			name: item.split('.')[1] || item.split('/')[1] || item,
+			dir: item,
+			outDir: item.split('.')[1] ? item.split('/')[0] + "/" + item.split('.')[1] : item
+		}
+	})
 }
 
 /**
@@ -47,9 +49,9 @@ const generateList = list => {
  * @param {String} dir путь куда копировать
  */
 const copyrFiles = (path, ext, dir) => {
-    copy([path + "**/*." + ext, dir], true, function (err) {
-        if (err) console.error(err);
-    });
+	copy([path + "**/*." + ext, dir], true, function (err) {
+		if (err) console.error(err);
+	});
 }
 
 /**
@@ -60,75 +62,88 @@ const copyrFiles = (path, ext, dir) => {
  * @returns {string}
  */
 const editContentMD = (contentMD, item, dateCreate) => {
-    const pathImg = `/assets/img/${item.name}/`;
-    let {metadata, content} = parseMD(contentMD);
-    let datePub = dateCreate;
+	const pathImg = `/assets/img/${item.name}/`;
+	let {metadata, content} = parseMD(contentMD);
+	let datePub = dateCreate;
 
-    if(metadata.publish_date) {
-        const arrDateAndTime = metadata.publish_date.split(' ')
-        const arrDate = arrDateAndTime[0].split('-');
-        const formatedDate = `${arrDate[2]}-${arrDate[1]}-${arrDate[0]} ${arrDateAndTime[1]}`;
-        datePub = dayjs(formatedDate).format('YYYY-MM-DDTHH:mm');
-    }
+	if(metadata.publish_date) {
+		const arrDateAndTime = metadata.publish_date.split(' ')
+		const arrDate = arrDateAndTime[0].split('-');
+		const formatedDate = `${arrDate[2]}-${arrDate[1]}-${arrDate[0]} ${arrDateAndTime[1]}`;
+		datePub = dayjs(formatedDate).format('YYYY-MM-DDTHH:mm');
+	}
 
-    const newMetadata = {
-        title: metadata.title,
-        longtitle: metadata.gtitle || "",
-        description: metadata.metadata ? metadata.metadata.description : "",
-        published: true,
-        date: datePub,
-        img: "/assets/img/img-none.png"
-    };
+	const newMetadata = {
+		title: metadata.title,
+		longtitle: metadata.gtitle || "",
+		description: metadata.metadata ? metadata.metadata.description : "",
+		published: true,
+		date: datePub,
+		img: "/assets/img/img-none.png"
+	};
 
-    if (metadata.media_order) {
-        newMetadata.img = pathImg + metadata.media_order.split(',')[0];
-    } else {
-        const pathOldImg = `grav/${item.dir}/`;
-        const imgPrimary = getListImagesInDir(pathOldImg)[0];
-        newMetadata.img = imgPrimary ? pathImg + imgPrimary : "";
-    }
+	if (metadata.media_order) {
+		newMetadata.img = metadata.media_order.split(',')[0];
+	} else {
+		const pathOldImg = `pages/${item.dir}/`;
+		const imgPrimary = getListImagesInDir(pathOldImg)[0];
+		newMetadata.img = imgPrimary ? imgPrimary : "";
+	}
 
-    if (metadata.metadata && metadata.metadata.keywords) {
-        newMetadata.keywords = metadata.metadata.keywords;
-    }
+	if(newMetadata.img) {
+		const pathOldImg = path.join(__dirname, `pages/${item.dir}/`);
 
-    const outMetadata = json2yaml(newMetadata).replace(/\n  /g, '\n') + "---\n";
+		fs.copyFile(pathOldImg+newMetadata.img, pathNuxt.posters+"/"+newMetadata.img, (err) => {
+			if (err) {
+				console.log("Error Found:", err);
+			}
+			else {
+				console.log("Постер скопирован: " + newMetadata.img );
+			}
+		});
+	}
 
-    content = content
-        // фикс заголовков
-        .replace(/(##)([\wА-Яа-я])/g, (whole, a, b) => a + " " + b)
-        // фикс путей изображений
-        .replace(/(!\[.*?\]\()(.+?)(\))/g, (whole, a, b, c) => a + pathImg + b.split('?classes')[0] + c);
+	if (metadata.metadata && metadata.metadata.keywords) {
+		newMetadata.keywords = metadata.metadata.keywords;
+	}
 
-    return outMetadata + content;
+	const outMetadata = json2yaml(newMetadata).replace(/\n  /g, '\n') + "---\n";
+
+	content = content
+		// фикс заголовков
+		.replace(/(##)([\wА-Яа-я])/g, (whole, a, b) => a + " " + b)
+		// фикс путей изображений
+		.replace(/(!\[.*?\]\()(.+?)(\))/g, (whole, a, b, c) => a + pathImg + b.split('?classes')[0] + c);
+
+	return outMetadata + content;
 }
 
 const listDirs = getDirectories(pathGrav);
 let listArticles = [];
 
 listDirs.forEach(dir => {
-    fs.mkdirSync(path.join(pathNuxt.content, dir.replace(pathGrav + "/", '')), { recursive: true })
-    listArticles = [
-        ...listArticles,
-        ...getDirectories(dir)
-    ]
+	fs.mkdirSync(path.join(pathNuxt.content, dir.replace(pathGrav + "/", '')), { recursive: true })
+	listArticles = [
+		...listArticles,
+		...getDirectories(dir)
+	]
 });
 
 listArticles = listArticles.map(path => path.replace(pathGrav + "/", ''));
 const list = generateList(listArticles);
 
 list.forEach(item => {
-    const pathItem = path.join(pathGrav, item.dir);
-    const pathImages = path.join(pathNuxt.img, item.name);
-    const pathFile = path.join(pathNuxt.content, item.outDir + ".md");
+	const pathItem = path.join(pathGrav, item.dir);
+	const pathImages = path.join(pathNuxt.img, item.name);
+	const pathFile = path.join(pathNuxt.content, item.outDir + ".md");
 
-    const contentMdIn = fs.readFileSync(path.join(pathItem, "page.md"), "utf8");
-    const contentMdOut = editContentMD(contentMdIn, item, birthDate(pathFile, 'YYYY-MM-DDTHH:mm:ss'));
+	const contentMdIn = fs.readFileSync(path.join(pathItem, "page.md"), "utf8");
+    const contentMdOut = editContentMD(contentMdIn, item, birthDate(path.join(pathItem, "page.md"), 'YYYY-MM-DDTHH:mm:ss'));
 
-    copyrFiles(pathItem, "png", pathImages);
-    copyrFiles(pathItem, "jpg", pathImages);
-    copyrFiles(pathItem, "jpeg", pathImages);
-    copyrFiles(pathItem, "gif", pathImages);
-    fs.writeFileSync(pathFile, contentMdOut);
-    console.log("success:", pathFile.replace(pathNuxt.content + "/", ''));
+	copyrFiles(pathItem, "png", pathImages);
+	copyrFiles(pathItem, "jpg", pathImages);
+	copyrFiles(pathItem, "jpeg", pathImages);
+	copyrFiles(pathItem, "gif", pathImages);
+	fs.writeFileSync(pathFile, contentMdOut);
+	console.log("success:", pathFile.replace(pathNuxt.content + "/", ''));
 });
